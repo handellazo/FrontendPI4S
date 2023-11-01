22f9059bfc255936f8f8aad829adcd484b81fe41
joshua12cx <luisangelticona575@gmail.com>
2023-11-01 18:51:25 -0500

    menuzzz

--- src/app/modules/Control/control.module.ts
+++ src/app/modules/Control/control.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ControlRoutingModule } from './control-routing.module';
+import { ControlComponent } from './pages/control/control.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ControlComponent
+  ],
   imports: [
     CommonModule,
     ControlRoutingModule
